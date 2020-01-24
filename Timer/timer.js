04862302f52933e9781f29cc832ce3370b5ec02d
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-01-24 18:53:26 +0200

    js olion muuttujiin käsiksi pääsy kokeilu

--- Timer/timer.js
+++ Timer/timer.js
@@ -1,6 +1,11 @@
-window.onload = function() {};
+window.onload = function() {
+    var timer = new Timer();
+    timer.start();
+};
 
 function Timer() {
     this.sec = 0;
-    
+    this.start = function() {
+        //console.log(this.sec);
+    };
 }
