12067cb7aed7ef168b7461d98e95b9aa52fee53d
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-01-24 20:30:22 +0200

    uusi muotoilu

--- Timer/timer.js
+++ Timer/timer.js
@@ -1,18 +1,38 @@
 window.onload = function() {
-    var timer = new Timer();
-    timer.start();
+
 };
 
-function Timer() {
+function Timer(element) {
+    /*
     this.millis = 0;
     this.seconds = 0;
     this.minutes = 0;
+    this.on = false;
+    */
+    this.element = element;
+    var time = ["0","00","00"];
+    var timeOut;
 
-    this.start = function() {
-        //console.log(this.seconds);
+    this.startTime = function() {
+        if(!this.on) {
+            //console.log(this.seconds);
+            this.on = true;
+            timeCount();
+        }
     };
     
-    this.stop = function() {
-
+    this.stopTime = function() {
+        if(this.on) {
+            clearTimeout(timeOut);
+            this.on = false;
+        }
     };
+
+    function timeCount() {
+        // tänne tulee elementin sisällön päivitys ja ajan näyttäminen
+        if(!element.hasAttribute("value")) {
+            element.innerHTML = time.toString.replace(",", ":");
+        }
+        timeOut = setTimeout(timeCount(), 10);
+    }
 }
